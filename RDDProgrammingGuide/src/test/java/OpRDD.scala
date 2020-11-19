cff71cb4bc9f16c273d8f8606de985afb9a3f94e
dominiczhu <dominiczhu@MacPro.local>
2020-11-19 14:32:15 +0800

    opRdd

---
+++ RDDProgrammingGuide/src/test/java/OpRDD.scala
@@ -0,0 +1,23 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+import org.junit.Test
+
+/**
+ * @author dominiczhu
+ * @date 2020/11/19 下午2:29
+ */
+class OpRDD {
+
+  val sparkConf: SparkConf = new SparkConf().setAppName("local").setMaster("local[2]")
+  val sc: SparkContext = new SparkContext(sparkConf)
+  val spark: SparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+
+  @Test
+  def emptyRddReduceByKey(): Unit = {
+    val data = Array((1,1), (2,2), (1,3))
+    val rdd = sc.parallelize(data).filter(t=>t._1<0)
+    println(rdd.collect().toSeq)
+
+    println(rdd.reduceByKey((a,b)=>a+b).collect().toSeq)
+  }
+}
