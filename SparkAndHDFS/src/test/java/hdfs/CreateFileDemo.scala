c6dbac23540df97613fa5402eea0752dbc5eabe1
dominiczhu <dominiczhu@tencent.com>
2020-11-23 14:23:12 +0800

    create file

--- SparkAndHDFS/src/test/java/hdfs/CreateFileDemo.scala
+++ SparkAndHDFS/src/test/java/hdfs/CreateFileDemo.scala
@@ -5,6 +5,7 @@ import java.io.IOException
 import org.apache.hadoop.fs.{FSDataOutputStream, FileSystem, Path}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
+import org.junit.Test
 
 /**
  * @author dominiczhu
@@ -14,6 +15,20 @@ class CreateFileDemo {
   implicit val sparkConf = new SparkConf().setAppName("local").setMaster("local[5]")
   implicit val sc = new SparkContext(sparkConf)
   implicit val spark = SparkSession.builder().config(sparkConf).getOrCreate()
+
+  @Test
+  def createEmptyFile():Unit={
+    val hdfs = FileSystem.get(sc.hadoopConfiguration)
+    hdfs.create(new Path("fake_hdfs/_SUCCESS"))
+  }
+
+  @Test
+  def createFileInDosentExistDir():Unit={
+//    集群测试，会自动创建父目录，如果父目录不存在的话
+    val path = new Path("fake_hdfs/doesnt_exist_dir/create_empty_file")
+    val hdfs = FileSystem.get(path.toUri,sc.hadoopConfiguration)
+    hdfs.create(path)
+  }
 
   def createHdfsFile(): Unit = {
     val path = new Path("hdfs://xxx/xxx")
